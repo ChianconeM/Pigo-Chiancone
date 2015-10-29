cc8e764a2357a7c71e75a350133fb2d53df9a1b9
Matt <ChianconeM@gilmour.org>
2015-10-29 11:10:31 -0400

    added methods

--- Pigo-Chiancone.py
+++ Pigo-Chiancone.py
@@ -1,11 +1,30 @@
-#from gopigo import *
+from gopigo import *
+import time
 
-__author__ = 'gilmour'
+__author__ = 'Matt'
+
 
 class Pigo:
+
+    isMoving =False
+    servoPos = 90
 
     def __init__(self):
         print "I'm such a pigo."
 
+    def stop(self):
+        self.isMoving = False
+        while stop() != 1:
+            time.sleep(.1)
+            print "I'm having trouble stopping"
+
+    def fwd(self):
+        self.isMoving = True
+        while fwd() != 1:
+            time.sleep(.1)
+            print "I can't seem to get going"
 
 bruh = Pigo()
+bruh.fwd()
+bruh.sleep(2)
+bruh.stop()
