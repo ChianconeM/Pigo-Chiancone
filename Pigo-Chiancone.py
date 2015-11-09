996eead6d246261dab9a450667832c075fce92f1
Matt <ChianconeM@gilmour.org>
2015-11-09 11:44:03 -0500

    added methods and allstop file

--- Pigo-Chiancone.py
+++ Pigo-Chiancone.py
@@ -1,7 +1,8 @@
 from gopigo import *
 import time
 
-__author__ = 'Matt'
+#Global variqavle on how close to something before stop
+STOP_DIST = 50
 
 
 class Pigo:
@@ -11,32 +12,64 @@ class Pigo:
     #####
 
     status = {'ismoving': False, 'servo': 90, 'leftspeed': 175,
-              'rightspeed': 175}
+              'rightspeed': 175, 'dist': 100}
 
     def __init__(self):
         print "I'm such a pigo."
+        self.checkDist()
 
     def stop(self):
-        self.isMoving = False
-        while stop() != 1:
-            time.sleep(.1)
-            print "I'm having trouble stopping"
+        self.status["isMoving"] = False
+        print "Whoaa there."
+        for x in range(3):
+            stop()
 
     def fwd(self):
-        self.isMoving = True
-        while fwd() != 1:
-            time.sleep(.1)
-            print "I can't seem to get going"
+        self.status['isMoving'] = True
+        print "lets roll"
+        for x in range(3):
+            fwd()
+
+    def bwd(self):
+        self.status['isMoving'] = True
+        print "back it up"
+        for x in range(3):
+            bwd()
+
+    def keepGoinng(self):
+        if self.status['dist'] < STOP_DIST:
+            return False
+        else:
+            return True
+
+    def checkDist(self):
+        self.status['dist'] = us_dist(15)
+        print "I see something " + str(self.status['dist']) + "mm away"
+        if not self.keepGoing():
+            print "Emergency stop from check distance method"
+            self.stop()
 
     #####
     #####  advanced methods
     #####
 
+    def dance(self):
+        print "I just want to dance"
+        if self.keepGoinng():
+            self.circleRight()
+            self.circleLeft()
+            self.shuffle()
+            self.servoShake()
+            self.blink()
+
 #####
 #####  main app starts here
 #####
 
-bruh = Pigo()
-bruh.fwd()
-bruh.sleep(2)
-bruh.stop()
+tina = Pigo()
+while tina.keepGoing():
+    tina.fwd()
+    tina.sleep(2)
+    tina.stop()
+
+tina.stop()
